abef66d0bf403bf25c36f0e93ffc1ae8e9b627dd
Tracy Tran <tracytran1189@gmail.com>
2021-11-22 23:04:23 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,21 @@ var playerMoney = 10;
 //You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
+
+
+    for (var i = 0; i < enemyNames.length; i++) {
+        console.log(enemyNames[i]);
+        console.log(i);
+        console.log(enemyNames[i] + " is at " + i + " index ");
+    }
+
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators")
 
@@ -24,14 +34,14 @@ var fight = function() {
 
         // Log a resulting message to the console so we know that it worked.
         console.log(
-            playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyNames + " . " + enemyNames + " now has " + enemyHealth + " health remaining."
         );
 
         // Check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         } else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         }
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
@@ -44,7 +54,7 @@ var fight = function() {
         }
         // Log a resulting message to the console so we know that it worked.
         console.log(
-            enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remainging."
+            enemyNames + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remainging."
         );
     }
     // if player choses to skip
@@ -67,5 +77,6 @@ var fight = function() {
     }
 
 };
-
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
